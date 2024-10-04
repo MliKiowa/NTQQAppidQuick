9af014e322510bc90532c8957ca83afc42d53291
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-04 19:49:53 +0800

    fix

--- GetAppid.js
+++ GetAppid.js
@@ -3,16 +3,15 @@ let findAppid = false;
 const waitModule = Process.platform == 'linux' ? 'qq' : 'QQNT.dll';
 const symbolGetString = "napi_get_value_string_utf8";
 async function main() {
-    send("Platform Moudle:" + waitModule);
-    //列出主模块所有导出函数
-    let exports = Module.enumerateExports(waitModule);
-    exports.forEach(function (ex) {
-        send(ex.name);
-    });
+    //let exports = Module.enumerateExports(waitModule);
+    // exports.forEach(function (ex) {
+    //     send(ex.name);
+    // });
     let hookPtr = Module.findExportByName(waitModule, symbolGetString);
     while (hookPtr == null) {
         hookPtr = Module.findExportByName(waitModule, symbolGetString);
     }
+    send("Hook Addr:" + hookPtr);
     Interceptor.attach(hookPtr, {
         onEnter(args) {
             this.data = args[2];
