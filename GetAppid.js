516709f2ae3d1a82995eb49e8f13f75749a701e6
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-04 19:36:05 +0800

    fix

--- GetAppid.js
+++ GetAppid.js
@@ -2,14 +2,12 @@ let findAppid = false;
 
 const waitModule = Process.platform == 'linux' ? 'qq' : 'QQNT.dll';
 const symbolGetString = "napi_get_value_string_utf8";
-send("Hooking");
 async function main() {
+    send("Platform Moudle:" + waitModule);
     let hookPtr = Module.findExportByName(waitModule, symbolGetString);
     while (hookPtr == null) {
         hookPtr = Module.findExportByName(waitModule, symbolGetString);
     }
-    console.log("Platform Module:", waitModule);
-    send("Platform Module:" + waitModule);
     Interceptor.attach(hookPtr, {
         onEnter(args) {
             this.data = args[2];
