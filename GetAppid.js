7a69749f871a164199ec9a7af727fc873634c828
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-04 20:00:19 +0800

    fix

--- GetAppid.js
+++ GetAppid.js
@@ -2,11 +2,7 @@ let findAppid = false;
 
 const waitModule = Process.platform == 'linux' ? 'qq' : 'QQNT.dll';
 const symbolGetString = "napi_get_value_string_utf8";
-async function main() {
-    //let exports = Module.enumerateExports(waitModule);
-    // exports.forEach(function (ex) {
-    //     send(ex.name);
-    // });
+async function mainWin() {
     let hookPtr = Module.findExportByName(waitModule, symbolGetString);
     while (hookPtr == null) {
         hookPtr = Module.findExportByName(waitModule, symbolGetString);
@@ -25,5 +21,25 @@ async function main() {
         }
     });
 }
-
-main().then();
+async function mainLinux() {
+    let exports = Module.enumerateExports(waitModule);
+    let hookPtr = exports.find(exp => exp.name == symbolGetString).address;
+    send("Hook Addr:" + hookPtr);
+    Interceptor.attach(hookPtr, {
+        onEnter(args) {
+            this.data = args[2];
+        },
+        onLeave(retval) {
+            let data = Memory.readUtf8String(this.data).substring(0, 10);
+            if (!findAppid && /^\d+$/.test(data) && data.startsWith("5")) {
+                send(data);
+                findAppid = true;
+            }
+        }
+    });
+}
+if (Process.platform == 'linux') {
+    mainLinux().then();
+} else {
+    mainWin().then();
+}
