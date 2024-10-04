85cdeec656d94dc20c199cadcea57eb9517b1118
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-04 19:22:45 +0800

    fix

--- GetAppid.js
+++ GetAppid.js
@@ -1,63 +1,27 @@
-const frida = require('frida');
-const fs = require('fs');
+let findAppid = false;
 
+const waitModule = Process.platform == 'linux' ? 'qq' : 'QQNT.dll';
+const symbolGetString = "napi_get_value_string_utf8";
+send("Hooking");
 async function main() {
-    const pid = await frida.spawn({
-        program: "qq",
-        env: { "display": ":1" }
-    });
-    const session = await frida.attach(pid);
-    const script = await session.createScript(`
-        let findAppid = false;
-
-        const waitModule = Process.platform == 'linux' ? 'qq' : 'QQNT.dll';
-        const symbolGetString = "napi_get_value_string_utf8";
-        send("Hooking");
-        async function main() {
-            let hookPtr = Module.findExportByName(waitModule, symbolGetString);
-            while (hookPtr == null) {
-                hookPtr = Module.findExportByName(waitModule, symbolGetString);
+    let hookPtr = Module.findExportByName(waitModule, symbolGetString);
+    while (hookPtr == null) {
+        hookPtr = Module.findExportByName(waitModule, symbolGetString);
+    }
+    console.log("Platform Module:", waitModule);
+    send("Platform Module:" + waitModule);
+    Interceptor.attach(hookPtr, {
+        onEnter(args) {
+            this.data = args[2];
+        },
+        onLeave(retval) {
+            let data = Memory.readUtf8String(this.data).substring(0, 10);
+            if (!findAppid && /^\d+$/.test(data) && data.startsWith("5")) {
+                send(data);
+                findAppid = true;
             }
-            console.log("Platform Module:", waitModule);
-            send("Platform Module:" + waitModule);
-            Interceptor.attach(hookPtr, {
-                onEnter(args) {
-                    this.data = args[2];
-                },
-                onLeave(retval) {
-                    let data = Memory.readUtf8String(this.data).substring(0, 10);
-                    if (!findAppid && /^\d+$/.test(data) && data.startsWith("5")) {
-                        send(data);
-                        findAppid = true;
-                    }
-                }
-            });
-        }
-
-        main().then();
-    `);
-
-    script.on('message', message => {
-        if (message.type === 'send') {
-            console.log('Appid:', message.payload);
-            fs.writeFileSync('./appid.txt', message.payload);
-            // 结束进程
-            process.exit(0);
         }
     });
-
-    script.load().then(() => {
-        console.log('Script loaded');
-    }).catch(err => {
-        console.error(err);
-    });
-
-    require('./application/app_launcher/index.js');
-    setTimeout(() => {
-        global.launcher.installPathPkgJson.main = "./application/app_launcher/index.js";
-    }, 0);
 }
 
-main().catch(err => {
-    console.error(err);
-});
+main().then();
